9f402da897e6e5549f0cd7c66f955475804789a9
maxmax <>
2017-09-29 06:13:16 +0300

    days test

--- routes/api/fake.js
+++ routes/api/fake.js
@@ -36,4 +36,42 @@ router.get('/times', function(req, res) {
   res.send(result);
 });
 
+router.get('/days', function(req, res) {
+  res.json([
+    {
+      "mo": [
+        {
+          "bt": 240,
+          "et": 779
+        }
+      ],
+      "tu": [],
+      "we": [],
+      "th": [
+        {
+          "bt": 240,
+          "et": 779
+        },
+        {
+          "bt": 1140,
+          "et": 1319
+        }
+      ],
+      "fr": [
+        {
+          "bt": 660,
+          "et": 1019
+        }
+      ],
+      "sa": [
+        {
+          "bt": 0,
+          "et": 1439
+        }
+      ],
+      "su": []
+    }
+  ]);
+});
+
 module.exports = router;
